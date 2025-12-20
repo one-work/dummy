ce7ac7700837b5e207b4cc795bbd4f2e894fec4b
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-20 10:03:14 +0800

    asets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,9 @@
 Rails.configuration.assets.paths += [
-  Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts'),
   Rails.root.join('app/assets/images'),
-  RailsDesign::Engine.root.join('app/assets/images')
+  Rails.root.join('app/assets/builds')
 ]
 
-Rails.configuration.assets.excluded_paths << ActionText::Engine.root.join('app/assets/javascripts') if defined? ActionText
-Rails.configuration.assets.excluded_paths << ActionView::Railtie.root.join('lib/assets/compiled') if defined? ActionView
+Rails.configuration.assets.excluded_paths += [
+  ActionText::Engine.root.join('app/assets/javascripts'),
+  ActionView::Railtie.root.join('lib/assets/compiled')
+]
