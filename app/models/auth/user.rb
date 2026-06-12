547a1467f55a5f3b651a87bb1527c785b90c90de
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-12 18:38:23 +0800

    roled ext user

--- app/models/auth/user.rb
+++ app/models/auth/user.rb
@@ -3,5 +3,6 @@ module Auth
     include Model::User if defined? RailsAuth
     include Notice::Ext::User if defined? RailsNotice
     include Org::Ext::User if defined? RailsOrg
+    include Roled::Ext::User
   end
 end
